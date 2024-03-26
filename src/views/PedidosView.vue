dddf814cb1546c9ce06be60fd296fd236831aeb5
Tainna <tainnaandryelli@gmail.com>
2024-03-26 11:18:17 -0300

    feat: Dashboard Component

--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="main-container">
-    <h1>PEDIDOS</h1>
+    <h1>Gerenciar Pedidos:</h1>
+    <Dashboard />
   </div>
 </template>
+<script>
+import Dashboard from "../components/Dashboard";
+
+export default {
+  name: "Pedidos",
+  components: {
+    Dashboard,
+  },
+};
+</script>
